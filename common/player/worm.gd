e5af6c9c8906b0a03b86ae9e6188895219558e4a
TwinSouls <BeyondTwinSouls@gmail.com>
2024-01-27 00:59:58 -0800

    movement 2
    Reworked movement so that it now actually works. Also added a timer to make the movement choppy and a basic stage for testing.

--- common/player/worm.gd
+++ common/player/worm.gd
@@ -1,52 +1,43 @@
 extends Area2D
-@export var speed = 10;
 var screen_size;
+
+# Movement variables.
+var none = Vector2(0, 0);
+var up = Vector2(0, -1);
+var down = Vector2(0, 1);
+var left = Vector2(-1, 0);
+var right = Vector2(1, 0);
+var move_dir: Vector2;
+var can_move: bool;
+
+func set_move_dir():
+	if Input.is_action_pressed("move_left") && move_dir != right:
+		move_dir = left;
+	if Input.is_action_pressed("move_right") && move_dir != left:
+		move_dir = right;
+	if Input.is_action_pressed("move_up") && move_dir != down:
+		move_dir = up;
+	if Input.is_action_pressed("move_down")&& move_dir != up:
+		move_dir = down;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Ticker.start();
+	move_dir = none;
 	screen_size = get_viewport_rect().size;
-	pass # Replace with function body.
-	
-# Makes sure the player isn't able to do a 180.
-func _get_x_direction(velocity: Vector2, x: int):
-	var old_x = velocity.x;
-	
-	if x != 0 && (x > old_x || x < old_x):
-		return old_x;
-	else: return x;
-	
-func _get_y_direction(velocity: Vector2, y: int):
-	var old_y = velocity.y;
-	
-	if y != 0 && (y > old_y || y < old_y):
-		return old_y;
-	else: return y;
-	
-	
-# Used to reset and update the player movement based on input.
-func _update_movement(velocity: Vector2, x: int, y: int):
-	velocity.x = _get_x_direction(velocity, x);
-	velocity.y = _get_y_direction(velocity, y);
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO;
 	
-	if velocity == Vector2.ZERO:
-		velocity.x = 1;
-	if Input.is_action_pressed("move_left"):
-		_update_movement(velocity, -1, 0);
-	if Input.is_action_pressed("move_right"):
-		_update_movement(velocity, 1, 0);
-	if Input.is_action_pressed("move_up"):
-		_update_movement(velocity, 0, -1);
-	if Input.is_action_pressed("move_down"):
-		_update_movement(velocity, 0, 1);
+	set_move_dir();
+	
+	if !can_move: return;
+	if move_dir == none: move_dir = right;
 		
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed;
-		
-	position += velocity * delta;
+	position += move_dir;
 	position = position.clamp(Vector2.ZERO, screen_size);
-	
-	pass
+	can_move = false;
+
+
+func _on_timer_timeout():
+	can_move = true;
