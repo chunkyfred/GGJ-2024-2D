9d21ffd5553c0991d00e6418eef9135c8325e4a7
chunkyfred <bidlocs@gmail.com>
2024-01-27 14:33:17 -0800

    Visual Novel Is Happening
    sorry about my placeholders

--- TalkScript.gd
+++ TalkScript.gd
@@ -1,7 +1,11 @@
 extends Control
 
 @onready var tm = $LettersTimer
-@onready var db = $DialogueBox
+@onready var db = $Panel/DialogueBox
+@onready var back = $Background
+
+@onready var first = $CharacterLeft
+@onready var second = $CharacterRight
 
 signal pressedSpace
 
@@ -9,12 +13,12 @@ var isFinished = false
 var buffer = false
 var skipTimer = false
 
-var dialoguestuff = ["This is a super cool test thing to say. \nI'm saying it now. \nIsn't this really super cool?", "I wish I was a bird."]
+var dialoguestuff = [["This is a super cool test thing to say. \nI'm saying it now. \nIsn't this really super cool?","res://benstuff/placeholder_emote1.png",0], ["I wish I was a bird.","res://benstuff/placeholder_emote2.png",0],["Me too, bud!","res://benstuff/placeholder_emote1.png",1]]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#tm.set_wait_time(.1)
-	bigTalky(dialoguestuff)
+	bigTalky(dialoguestuff, "res://benstuff/stupid.png")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -27,9 +31,14 @@ func _process(delta):
 			pressedSpace.emit()
 		buffer = false
 
-func talkytime(string):
+func talkytime(lineInf):
+	var icon = load(lineInf[1])
+	if !lineInf[2]:
+		first.texture = icon
+	else:
+		second.texture = icon
 	isFinished = false
-	for letter in string:
+	for letter in lineInf[0]:
 		if skipTimer:
 			db.add_text(letter)
 			continue
@@ -41,7 +50,9 @@ func talkytime(string):
 	
 	return
 
-func bigTalky(dlList):
+func bigTalky(dlList,bg):
+	var bgFile = load(bg)
+	back.texture = bgFile
 	for line in dlList:
 		db.clear()
 		await talkytime(line)
